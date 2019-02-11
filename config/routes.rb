272682b8565a03f712c7c63c4fa2926be75ffde5
koval <kovalsky.corp@gmail.com>
2019-02-11 10:18:37 +1000

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'employees', to: redirect('employees/show')
   get 'employees/show'
   get 'employees/json/:id/', to: 'employees#showAssistants'
 
-  resources :employees
+  resources :employees, only: [:show]
 
   root 'employees#show'
+
+  get "/employees/:id/(*url)", to: redirect('/404')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
